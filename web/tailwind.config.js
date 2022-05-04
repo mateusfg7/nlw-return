b2ed10b71757d29f1cdd7f89876c0db441e4e750
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-05-04 08:34:27 -0300

    web: add new color on tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
         brand: {
-          500: '#8257e6'
-        }
-      }
+          300: '#996DFF',
+          500: '#8257e6',
+        },
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('tailwind-scrollbar')
-  ],
+  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')],
 }
