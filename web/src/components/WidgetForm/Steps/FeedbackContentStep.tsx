5aa20861ca8b7e83f7b5674ab8965dea1a8c0bc0
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-05-04 10:41:13 -0300

    web: create a `handleSubmitFeedback` function

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 import { ArrowLeft } from 'phosphor-react'
 
@@ -16,8 +16,18 @@ export function FeedbackContentStep({
   onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [comment, setComment] = useState('')
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault()
+
+    console.log({
+      screenshot,
+      comment,
+    })
+  }
 
   return (
     <>
@@ -42,10 +52,11 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <form className='my-4 w-full'>
+      <form className='my-4 w-full' onSubmit={handleSubmitFeedback}>
         <textarea
           className='min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'
           placeholder='Conte com detalhes o que esta acontecendo...'
+          onChange={(event) => setComment(event.target.value)}
         />
 
         <footer className='flex gap-2 mt-2'>
