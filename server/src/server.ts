4bdae3a885c2ecba7a32adb5fa80de4f1a85bc91
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-05-16 21:03:07 -0300

    server: use nodemailer with mailtrap

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'cdf4563435318c',
+    pass: '6a60bca7a2da9f',
+  },
+})
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     },
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Mateus Felipe <mateusfelipefg77@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #333;">`,
+      `<p>Tipo do feedback: <spam style="color: #000">${type}</spam></p>`,
+      `<p>Comentário: <spam style="color: #000">${comment}</spam></p>`,
+      `</div>`,
+    ].join('\n'),
   })
 
   return res.status(201).json({
